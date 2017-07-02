8a8c3cb989cfaf6cd3148a49f17ea8cb0bd94dc6
Alexander Rao <alexhrao@gmail.com>
2017-07-02 01:52:07 -0400

    Update onReady.js to account for variable height navbar

--- resources/scripts/onReady.js
+++ resources/scripts/onReady.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top - 50
+                scrollTop: $(hash).offset().top - $(".navbar-fixed-top").top
             }, 800, function() {
                 window.location.hash = hash;
             });
