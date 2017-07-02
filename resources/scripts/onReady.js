00c0f900fe8a92d1cfc9367db569bd90943c9959
Alexander Rao <alexhrao@gmail.com>
2017-07-02 02:07:09 -0400

    Hotfix for broken jQuery

--- resources/scripts/onReady.js
+++ resources/scripts/onReady.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top - $(".navbar-fixed-top").top
+                scrollTop: $(hash).offset().top - 50
             }, 800, function() {
                 window.location.hash = hash;
             });
