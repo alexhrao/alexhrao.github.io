d62f0d09dccbf6c32405acae5654f6f81904505c
Alexander Rao <alexhrao@gmail.com>
2017-07-12 16:17:30 -0400

    Add contact form

--- resources/scripts/onReady.js
+++ resources/scripts/onReady.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    emailjs.init("user_F0kE6IBMQ9Lm6OLpQRxDf")
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
     $("#menuBar a").on('click', function(event) {
@@ -29,7 +30,19 @@ $(document).ready(function() {
         maxHeight: null,
         focus: false
     });
-    $('#sendMessage').click(function(event) {
-        alert("Sup");
-    })
+    $('#sendMessage').click(function() {
+        alert($('#contactName').val());
+        emailjs.send("gmail", "default", {name: $('#contactName').val(), email: $('#contactEmail').val(), content: $('#messageContent').summernote('code')})
+        .then(function(response) {
+            $('#messenger').show();
+            $('#messenger').append("<div id='messageSent' class='alert alert-success alert-dismissable'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Success!</strong> Your message was successfully delivered!</div>").delay(2500).fadeOut(1000, function() {
+                $('#messenger').empty();
+            });
+        }, function(err) {
+            $('#messenger').show();
+            $('#messenger').append("<div id='messageSent' class='alert alert-danger alert-dismissable'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Drats!</strong> We couldn't send your message. Please try again later.</div>").delay(2500).fadeOut(1000, function() {
+                $('#messenger').empty();
+            });
+        });
+    });
 });
