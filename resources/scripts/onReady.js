11bcb212936f2206650a80f32df29786d59bd2e3
Alexander Rao <alexhrao@gmail.com>
2017-07-13 09:25:16 -0400

    Remove async from ajax

--- resources/scripts/onReady.js
+++ resources/scripts/onReady.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     emailjs.init("user_F0kE6IBMQ9Lm6OLpQRxDf")
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
+    
     $("#menuBar a").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -13,6 +14,7 @@ $(document).ready(function() {
             });
         }
     });
+    
     $('#logoName').hide();
     $('#logo').hover(
         function() {
@@ -24,13 +26,41 @@ $(document).ready(function() {
             $('#logoImage').fadeIn(500);
         }
     );
+    
+    $.ajax({
+        url: 'https://api.github.com/emojis'
+    }).then(function(data) {
+        window.emojis = Object.keys(data);
+        window.emojiUrls = data;
+    });
+
     $('#messageEditor').summernote({
-        placeholder: 'Your message to me...',
+        placeholder: 'Your message to me. For emojis, type ":" followed by the name of the emoji.',
         height: 300,
         minHeight: null,
         maxHeight: null,
-        focus: false
+        focus: false,
+        hint: {
+            match: /:([\-+\w]+)$/,
+            search: function(key, call) {
+                call($.grep(emojis, function(item) {
+                    return item.indexOf(key) === 0;
+                }));
+            },
+            template: function(item) {
+                var content = emojiUrls[item];
+                return '<img src="' + content + '" width="20" /> :' + item + ':';
+            },
+            content: function(item) {
+                var url = emojiUrls[item];
+                if (url) {
+                    return $('<img />').attr('src', url).css('width', 20)[0];
+                };
+                return '';
+            }
+        }
     });
+    
     $('#sendMessage').click(function() {
         var dt = new Date($.now());
         emailjs.send("gmail", "default", {
@@ -44,6 +74,7 @@ $(document).ready(function() {
             $('#messenger').append("<div id='messageSent' class='alert alert-success alert-dismissable'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Success!</strong> Your message was successfully delivered!</div>").delay(2500).fadeOut(1000, function() {
                 $('#messenger').empty();
             });
+            $('#messageEditor').summernote 
         }, function(err) {
             $('#messenger').show();
             $('#messenger').append("<div id='messageSent' class='alert alert-danger alert-dismissable'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Drats!</strong> We couldn't send your message. Please try again later.</div>").delay(2500).fadeOut(1000, function() {
